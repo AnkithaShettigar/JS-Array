7850d08e65705d74ad262b89b8b3e60dbe3889cf
AnkithaShettigar <ankitha.yermal@gmail.com>
2022-08-12 13:39:22 +0530

    Added Arrays

---
+++ PresentOrNot.js
@@ -0,0 +1,18 @@
+const Find_Num = (array,N,M) => 
+{
+ 
+  // for(let i=0;i<N;i++){
+  //   if(array[i]===M){
+  //     return "YES"
+  //   }
+      
+  // }
+  // return "No"
+  if(array.includes(M)){
+    return "YES"
+  } 
+  else{
+  return "NO"
+  }
+};
+ 
---
+++ incrementArray.js
@@ -0,0 +1,10 @@
+
+const Inc_Arr = (array,N) => 
+{
+  
+ array=array.map(item => item+1);
+ let a=array.join(' ');
+ console.log(a)
+
+};
+ 
